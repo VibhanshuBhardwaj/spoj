e55fe62c5f6c90fbac3b1cc00bd5d6d3cd5e655a
Vibhanshu Bhardwaj <vbhardwaj@yahoo-inc.com>
2017-07-05 19:40:40 -0700

    clenup

--- FCTRL2.cpp
+++ FCTRL2.cpp
@@ -2,15 +2,24 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <cmath>
+#include <algorithm>
 using namespace std;
 
 class BigNumber {
-	vector<int> *arr;
+	
 public:
+	vector<int> *arr;
 	BigNumber() {
 	//	cout << "ctor called " << endl;
 		arr = new vector<int>();
 		//cout << arr->size() << endl;
+ 	}
+ 	int size() {
+ 		return arr->size();
+ 	}
+ 	int at(int i) {
+ 		return arr->at(i);
  	}
  	BigNumber(int n) {
  		arr = new vector<int>();
@@ -26,11 +35,11 @@ public:
  	//	cout<< "over heree" << endl;
  		
 
- 		cout << "new bignumber " << endl;
- 		this->prettyPrint();
+ 	//	cout << "new bignumber " << endl;
+ 		//this->prettyPrint();
  	}
 
- 	string mytoString(int input) {
+ 	string intToString(int input) {
 		ostringstream ss;
 		ss << input;
 		return ss.str();
@@ -45,60 +54,111 @@ public:
 		//cout << "FUCK";
 		//cout << "received char " << c << endl;
 	int ret = c - '0';
-	cout << "returning int " << ret << endl;
+	//cout << "returning int " << ret << endl;
 	return ret;
 	}
 	int modTen(BigNumber* b) {
 		int len = b->size();
 		return b->at(len-1);
 	}
+	BigNumber* get_tens(BigNumber* b) {
+		for (int i =1; arr->size(); i++) {
+			arr[i-1] = arr[i];
+		}
+		arr->pop_back();
+	}
+	// BigNumber* operator+(BigNumber* toAdd) {
+	// 	int smaller = min(arr->size(), toAdd->size());
+	// 	int carry=0;
+	// 	bool append =  false;
+	// 	for (int i = 0; i< smaller; i++) {
+	// 		int newval = arr->at(i) + toAdd->at(i);
+	// 		(*arr)[i] = (newval % 10) +  carry;
+	// 		carry =  newval / 10;
+	// 		if (i== smaller - 1 ) {
+	// 			while(carry!=0) {
+	// 				if (i >=arr->size()) {append=true; }
+	// 				newval = arr->at(i) + carry;
+	// 				(*arr)[i] = newval;
+	// 				carry =  newval / 10;
+	// 				i++; 
+	// 			}
+	// 		}
+	// 	}
+	// 	if (append) arr->insert(arr->begin(),1);
+	// 	return this;
+	// }
+	BigNumber* operator*(BigNumber* b) {
+		for (int i =0; i< b->size(); i++) {
+
+		}
+	}
  	BigNumber* operator*(int k) {
+ 		if (k>99){
+ 			cout <<"shouldn't happen ";
+ 			throw;
+ 		}
  		//cout << "inside opeartor k = " << k << endl;
  		int carry = 0;
  		int ogsize =  arr->size();
  		//cout << "ogsize " << ogsize << endl;
  		for (int i = 0 ; i < ogsize; i++) {
  			//cout <<"ar size " << ogsize << endl;
- 			BigNumber temp = BigNumber(1);
- 			temp = temp* ((*arr)[i]) ;
- 			temp = temp* k;
- 			int units_place = modTen(&temp);
- 			long long tens_place = temp / 10;
- 			cout << "old item at " <<i << "th position " << (*arr)[i] << endl;
- 			(*arr)[i] = units_place + carry;
- 			cout << "temp, units_place, tens_place " << temp << " " << units_place << " " << tens_place << endl;
- 			cout << "new item at " <<i << "th position " << (*arr)[i] << endl;
+ 			int digit_multip = this->at(i) * k ;
+ 			int net = digit_multip + carry;
+ 			int units_place = net % 10;
+ 			(*arr)[i] = units_place;
+ 			carry = net /10;
+
+
+ 			// int temp = ((*arr)[i]) * k ;
+ 			// temp += carry;
+ 			// int units_place =temp %10;
+ 			// int carry = temp - units_place;
+ 			// cout << "old item at " <<i << "th position " << (*arr)[i] << endl;
+ 			// (*arr)[i] = units_place + carry;
+ 			// //cout << "temp, units_place, tens_place " << temp << " " << units_place << " " << tens_place << endl;
+ 			// cout << "new item at " <<i << "th position " << (*arr)[i] << endl;
  		//	cout << "og i " << i << endl;
- 			if (i!=ogsize-1) { 
- 				carry= tens_place; //BITCH
- 			}
+ 			//if (i!=ogsize-1) { 
+ 			//	carry= tens_place; //BITCH
+ 			//}
  			
- 			else if (tens_place!=0){
- 				string toAppend = mytoString(tens_place);
- 				cout << "toAppend " << toAppend << endl;
- 				for (int i = toAppend.length()-1; i>=0; i--) {
- 					//cout << "i " << i <<endl;
- 					cout << "before append " << endl;
- 				this->prettyPrint();
- 			//		cout<< "before appending size " << arr->size() << endl;
- 			//		cout << "being appended to arr " << toAppend[i] << endl;
- 					arr->push_back(toInt(toAppend[i]));
- 					cout << "just after push_back " << endl;
- 			//		cout << "new size " << arr->size() << endl;
- 					this->prettyPrint();
- 				}
- 				//cout << "tens_place " <<  tens_place << endl;
- 				//while(tens_place/10 !=0 ){ 
- 				//	arr->push_back(tens_place/10);
- 				//	tens_place = tens_place % 10
- 				//}
- 			}
+ 			// else if (tens_place!=0){
+ 			// 	string toAppend = intToString(tens_place);
+ 			// 	cout << "toAppend " << toAppend << endl;
+ 			// 	for (int i = toAppend.length()-1; i>=0; i--) {
+ 			// 		//cout << "i " << i <<endl;
+ 			// 		cout << "before append " << endl;
+ 			// 	this->prettyPrint();
+ 			// //		cout<< "before appending size " << arr->size() << endl;
+ 			// //		cout << "being appended to arr " << toAppend[i] << endl;
+ 			// 		arr->push_back(toInt(toAppend[i]));
+ 			// 		cout << "just after push_back " << endl;
+ 			// //		cout << "new size " << arr->size() << endl;
+ 			// 		this->prettyPrint();
+ 			// 	}
+ 			// 	//cout << "tens_place " <<  tens_place << endl;
+ 			// 	//while(tens_place/10 !=0 ){ 
+ 			// 	//	arr->push_back(tens_place/10);
+ 			// 	//	tens_place = tens_place % 10
+ 			// 	//}
+ 			// }
 
  			//cout << (*arr)[i] << endl;
 
  		}
- 		cout << "intermediate result. just multiplied by " << k << endl;
- 		this->prettyPrint();
+ 		if (carry > 0) {
+ 			//cout <<"carry " << carry <<endl;
+ 			//digits need to be increased
+ 			string sval = intToString(carry);
+ 			for (int i = 0; i < sval.length(); i++) {
+ 				arr->push_back(toInt(sval[i]));
+ 				//arr->insert(arr->begin(),toInt(sval[i]));
+ 			}
+ 		}
+ 		//cout << "intermediate result. just multiplied by " << k << endl;
+ 		//this->prettyPrint();
  		return this;
  	}
  	void print() {
@@ -123,7 +183,7 @@ void removeFirstLine() {
 	string trash;
 	cin >> trash;
 }
-string mytoString(int input) {
+string intToString(int input) {
 	ostringstream ss;
 	ss << input;
 	return ss.str();
@@ -136,9 +196,9 @@ int toInt(string s) {
 	return r;
 }
 int toInt(char c) {
-	cout << "received char " << c << endl;
+	//cout << "received char " << c << endl;
 	int ret = c - '0';
-	cout << "returning int " << ret << endl;
+	//cout << "returning int " << ret << endl;
 	return ret;
 	//cout <<
 }
@@ -157,7 +217,7 @@ int main() {
 		BigNumber * bn = factorial(x);
 		//BigNumber * bn = new BigNumber(336);
 		//bn = (*bn)*5;
-		cout << "final answer ";
+		//cout << "final answer ";
 		bn->prettyPrint();
 		delete bn;
 		//bn
