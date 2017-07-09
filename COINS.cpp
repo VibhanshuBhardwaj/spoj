320619eeb754f2d58e327ef7e5578f344ee6911c
Vibhanshu Bhardwaj <bhardwajvibhanshu@gmail.com>
2017-07-09 13:01:36 -0700

    solved COINS.

---
+++ COINS.cpp
@@ -0,0 +1,64 @@
+//
+// Created by Vibhanshu Bhardwaj on 7/9/17.
+//
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+void removeFirstLine() {
+    string trash;
+    cin >> trash;
+}
+
+
+string toString(int input) {
+    ostringstream ss;
+    ss << input;
+    return ss.str();
+}
+
+int toInt(string s) {
+    istringstream ss(s);
+    int r;
+    ss >> r;
+    return r;
+}
+int toInt(char c) {
+    return c - '0';
+}
+int maxVal(int c) {
+    return c;
+}
+//create an array of ints. index represents the denomination of the coin and the value is the maximum amt of $$.
+
+int main() {
+    unsigned int n;
+    while (cin >> n) {
+        unsigned int *arr = new unsigned int[n+1];
+        for (unsigned int i =0 ;i <= n; i++) {
+            arr[i] = i;
+            unsigned int half = i /2;
+            unsigned int third = i/3;
+            unsigned int fourth = i/4;
+            auto sum = arr[half] + arr[third] + arr[fourth];
+            //cout << "max sum if broken " << sum << endl;
+            if (sum > i) { //NOT sum > n
+                arr[i] = sum;
+
+            }
+
+        }
+        //for(long i =0; i <= n; i++) cout << arr[i] << " ";
+        //cout << endl;
+        cout << arr[n] << endl;
+        delete[] arr;
+    }
+
+    return 0;
+}
